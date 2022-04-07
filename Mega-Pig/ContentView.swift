1847db1244618065de677dadcc76c3ed4c11d292
Inho Choi <eknow25@gmail.com>
2022-04-07 16:32:57 +0900

    Init Project

---
+++ Mega-Pig/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mega-Pig
+//
+//  Created by Choi Inho on 2022/04/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mega-Pig/Mega_PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mega_PigApp.swift
+//  Mega-Pig
+//
+//  Created by Choi Inho on 2022/04/07.
+//
+
+import SwiftUI
+
+@main
+struct Mega_PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
