594e8a0da5dec42b8fc04318e06a483f2b5413b1
jeonghyeon Hwang <hijihilin@gmail.com>
2022-04-10 00:57:23 +0900

    [FEAT] Add Daily View Layout

--- Mega-Pig/DailyView.swift
+++ Mega-Pig/DailyView.swift
@@ -10,13 +10,61 @@ import SwiftUI
 struct DailyView: View {
     @State var ymd:String = "2XXX.01.04"
     
-    var body: some View {
-        NavigationView{
-                    Text("Daily View")
+    func SizeIs(a: CGFloat, b: CGFloat) -> CGFloat{
+        if(a > b)
+        {
+            return b * 0.9
         }
-        .navigationTitle(ymd)
-        .navigationViewStyle(StackNavigationViewStyle())
-        .navigationBarTitleDisplayMode(.inline)
+        else
+        {
+            return a * 0.9
+        }
+    }
+    var body: some View {
+        GeometryReader { geometry in
+            let width = geometry.size.width
+            let height = geometry.size.height
+            let topHeight = height * 0.13
+            let bottomHeight = height * 0.87
+            let bottomTHeight = bottomHeight * 0.4
+            let bottomBHeight = bottomHeight * 0.6
+            let graphWidth = width * 0.9
+            
+                VStack(spacing: 0){
+                    NavigationView(){
+                    }
+                        .navigationTitle(ymd)
+                        .navigationViewStyle(StackNavigationViewStyle())
+                        .navigationBarTitleDisplayMode(.inline)
+                            .background(Color.blue)
+                    VStack(){
+                        Text("Week Calendar")
+                    }.frame(width: width, height: topHeight, alignment: .center)
+                    VStack(spacing: 0){
+                        VStack (){
+                            Text("Pie Chart Area")
+                            Text("Bar Chart Area")
+                        }.frame(width: graphWidth, height: bottomTHeight, alignment: .center)
+                            .background(Color.gray)
+                        VStack(spacing: 0){
+                            VStack(){
+                                
+                            Text("Photo Area")
+                            }
+//                            .frame(width: SizeIs(a: graphWidth, b: bottomBHeight), height: SizeIs(a: graphWidth, b: bottomBHeight), alignment: .center)
+//                                .background(Color.red)
+                                .frame(width: graphWidth * 0.9, height: bottomBHeight * 0.9, alignment: .center)
+                                    .background(Color.red)
+                                
+                        }.frame(width: graphWidth, height: bottomBHeight, alignment: .center)
+                            .background(Color.brown)
+                        
+                    }.frame(width: width, height: bottomHeight, alignment: .center)
+                        .background(Color.teal)
+                        
+                }
+            }
+            
     }
 }
 
