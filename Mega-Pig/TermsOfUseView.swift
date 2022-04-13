ed9795f24455480f9b280cbf50b2c7c04b6c0170
jeonghyeon Hwang <hijihilin@gmail.com>
2022-04-13 23:14:49 +0900

    [FEAT] Add MyPage Navigation Link Views
    - Add EditView
    - Add TermsOfUseView

---
+++ Mega-Pig/TermsOfUseView.swift
@@ -0,0 +1,20 @@
+//
+//  TermsOfUseView.swift
+//  Mega-Pig
+//
+//  Created by 황정현 on 2022/04/13.
+//
+
+import SwiftUI
+
+struct TermsOfUseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TermsOfUseView_Previews: PreviewProvider {
+    static var previews: some View {
+        TermsOfUseView()
+    }
+}
