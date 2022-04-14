2dbb577214c3f5931939d467f023ef67052a1faa
jeonghyeon Hwang <hijihilin@gmail.com>
2022-04-14 11:02:24 +0900

    [FEAT] Add TermsOfUse View Layout

--- Mega-Pig/TermsOfUseView.swift
+++ Mega-Pig/TermsOfUseView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct TermsOfUseView: View {
+    let termsOfUse = "ABCDeidkdkdj ielskdjfiwldk fieksljaielfkj baaiejfkee kfjdisdkfjkjasdkfjwoiefdkjf. dijksudikm,difjawo eifjdkslfhril sidkfqoeidkfhsls diodfkdlfadjfao jidfs lkoiewfdakdf kloiefjfie eikdddddifjkjweoijf wiekdjflsiejfdkdkdjsidjfkj ieldkfj dkdiefjdkfj."
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            let width = geometry.size.width
+            ScrollView()
+            {
+                VStack
+                {
+                    ZStack
+                    {
+                        VStack(spacing: width * 0.005){
+                            ForEach(1..<21) {
+                                index in
+                                Text("제\(index)조").font(.system(size: 16)).frame(width: width * 0.84, alignment:.leading).padding(.top, width * 0.03)
+                                Text(termsOfUse).font(.system(size: 14))
+                                    .padding(.horizontal, width * 0.03)
+                                    .padding(.bottom, width * 0.03)
+                                    .frame(width: width * 0.92)
+                            }
+                        }.background(RoundedRectangle(cornerRadius: 10).stroke(Color.accentColor))
+                    }
+                }.frame(width: width * 0.95, alignment: .center)
+            }.frame(width: width, alignment: .center)
+        }.navigationTitle("앱 사용약관")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
